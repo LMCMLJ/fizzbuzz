f4046d7a5f509d38dbaddec513ebbfd6df65658c
LMCMLJ <mjcalvey2@gmail.com>
2016-09-24 18:21:44 +0100

    Adds final fizzbuzz code.

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,8 +1,9 @@
 def fizzbuzz(num)
-  if num % 3 == 0
-    answer = 'Fizz'
-  elsif num % 5 == 0
-    answer = 'Buzz'
+  if num % 3 == 0 && num % 5 != 0
+    'Fizz'
+  elsif num % 5 == 0 && num % 3 != 0
+    'Buzz'
+  elsif num % 3 == 0 && num % 5 == 0
+    'FizzBuzz'
   end
-  return answer
 end
